38d2ea2f44d1ce00d07cf1e01e78843ddf2c6a98
AlexZhuchinskiy <lokomotiv-kiev94@bigmir.net>
2014-06-25 11:48:42 +0300

    Add menu and file with rule

--- Game.cpp
+++ Game.cpp
@@ -25,6 +25,44 @@ Game::~Game(void)
 	
 }
 
-void Game :: startLabQuest(){
+void Game :: startQuest(){
+		
+}
+
+void Game :: goHome(){
+	file.accessToFile("goHome.txt");	
+}
+
+void Game :: comeToSwimmingPool(){
 	
+}
+
+void Game :: menu(){
+	cout << "Crocodile Advantages" << endl;
+	cout << "1.Start" << endl;
+	cout << "2.Rules" << endl;
+	cout << "3.Exit" << endl;
+	int ans = 0;
+	cin >> ans;
+	switch (ans)
+	{
+	case 1:
+		system("cls");
+		startQuest();
+		break;
+	case 2:
+		system("cls");
+		file.accessToFile("Rule.txt");
+		system("pause");
+		menu();
+		break;
+	case 3:
+		exit(0);
+		break;
+	default:
+		cout << "Error of choose ! For choose enter 1,2 or 3 ." << endl;
+		system("cls");
+		menu();
+		break;
+	}
 }
